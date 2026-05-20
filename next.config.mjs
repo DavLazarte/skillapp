7afca08982538f4cdf12c4a8680cad7cf7195bdc
DavLazarte <dav.lazarte@gmail.com>
2026-05-20 11:08:02 -0300

    oackaje

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,18 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
   },
-  serverExternalPackages: ["@libsql/client"],
+  serverExternalPackages: [
+    "@libsql/client",
+    "@prisma/adapter-libsql",
+    "@prisma/client",
+    "prisma",
+  ],
 }
 
 export default nextConfig
